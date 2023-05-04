289e318036c031cafb7f1a928da0ecd8c9742559
kk <+>
2023-05-04 22:17:13 +0300

    Solve routing issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import SearchForm from './SearchForm';
+import { NavLink } from 'react-router-dom';
+import SearchForm from "./SearchForm"
 import CocktailList from './CocktailList';
+import Comments from './comments';
 
 const Home = () => {
   return (
     <main>
       <SearchForm/>
       <CocktailList/>
+      <Comments/>
+      
+<section>
+<NavLink className='btn btn-primary' to ="/">Back Home</NavLink>
+</section>
     </main>
   )
 }
+
 
 export default Home;
